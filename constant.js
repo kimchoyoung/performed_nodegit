cfc8a3126e98e0d60da241d14dcb4759c09f3f43
soicem <knq512412@gmail.com>
2019-08-30 23:22:59 +0900

    init project

---
+++ constant.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+exports.tmp_dir = './tmp';
+// add_commit
+exports.add_commit_filename = 'add_commit.txt';
+
+exports.cur_git_path = './TestNodegit/.git';
+exports.test_git_path = './TestNodegit/.git';
+exports.test_file_path = './TestNodegit/';
+
+// git
+exports.commiter_name = 'soicem';
+exports.commiter_email = 'soicem@github.com';
+
+// get_branch
+exports.master_branch_name = 'master';
+exports.branch_name = 'test01';
+exports.checkout_branch = 'test_checkout';
+exports.new_branch_name = 'new_branch';
+
+// git account
+exports.account = 'ID';
+exports.password = 'PASSWD';
